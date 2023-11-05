d3fc1c8eb35e4fcefd26e2640b33d21740c093cd
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-11-05 23:41:46 +0100

    initialize project

---
+++ MicroServiceUser/MicroServiceUser/src/test/java/com/zakariae/MicroServiceUser/MicroServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakariae.MicroServiceUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
